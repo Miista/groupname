b1764b7d3f124a83e7d350a9c77faa2894fdb65f
Søren Palmund <palmund@gmail.com>
2015-09-07 10:21:03 +0200

    Initial work on Closest-Pair algorithm

--- HandIn3/src/CP.java
+++ HandIn3/src/CP.java
@@ -1,6 +1,28 @@
+import javafx.util.Pair;
+
+import java.awt.Point;
+import java.util.List;
+
 /**
  * Created by Søren Palmund on 07-09-2015.
  */
 public class CP
 {
+    public static Pair<Point, Point> ClosestPair(List<Point> points)
+    {
+        if (points.size() <= 3)
+        {
+            // Find closest pair
+        }
+        else
+        {
+            final int median = points.size() / 2;
+            final List<Point> left = points.subList( 0, median );
+            final List<Point> right = points.subList( median, points.size() );
+        }
+
+
+
+        return null;
+    }
 }

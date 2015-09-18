f2aa0f4ef0653c497a3ed9dd81d9fad85268d682
Søren Palmund <palmund@gmail.com>
2015-09-18 13:33:10 +0200

    CP: Reimplemented mergesort, formatted code

--- HandIn3/src/CP.java
+++ HandIn3/src/CP.java
@@ -6,7 +6,6 @@ import java.util.ListIterator;
 
 /**
  * Created by Soren Palmund on 07-09-2015.
- * @param <T>
  */
 public class CP
 {
@@ -70,63 +69,97 @@ public class CP
 	public static Tuple<Double, List<EPoint> > ClosestPair( List<EPoint> input )
 	{
 		Double delta;
-		switch ( input.size() )
+		switch (input.size())
 		{
-		case 3: // Only 3 EPoints - Base Case
-		return closestOf3( input );
-		case 2:
-		if( input.get(0).y > input.get(1).y ) Collections.swap(input, 0, 1);
-		delta = input.get(0).distance( input.get(1) );
-		return new Tuple<Double, List<EPoint> >(delta, input);
-		case 1: // This should not occur - only if the given list only has 1 point
-		System.out.println("Hit 1 element in queue");
-		System.exit( -2 );
-		return null;
-		default:
-			/**
-			 * We partition the input into 2 subsets.
-			 */
-			final int medianIndex = input.size() / 2;
-			final Tuple<Double, List<EPoint> > Q = ClosestPair( input.subList(0, medianIndex) );
-			final Tuple<Double, List<EPoint> > R = ClosestPair( input.subList(medianIndex, input.size()) );
+			case 3: // Only 3 EPoints - Base Case
+				return closestOf3( input );
+			case 2:
+				if (input.get( 0 ).y > input.get( 1 ).y) { Collections.swap( input, 0, 1 ); }
+				delta = input.get( 0 )
+							 .distance( input.get( 1 ) );
+				return new Tuple<Double, List<EPoint>>( delta, input );
+			case 1: // This should not occur - only if the given list only has 1 point
+				System.out.println( "Hit 1 element in queue" );
+				System.exit( -2 );
+				return null;
+			default:
+				/**
+				 * We partition the input into 2 subsets.
+				 */
+				final int medianIndex = input.size() / 2;
+				final Tuple<Double, List<EPoint>> Q = ClosestPair( input.subList( 0, medianIndex ) );
+				final Tuple<Double, List<EPoint>> R = ClosestPair( input.subList( medianIndex, input.size() ) );
 
-			/**
-			 * We find the smallest delta based on the subsets
-			 */
-			delta = Math.min(Q.val1, R.val1);
+				/**
+				 * We find the smallest delta based on the subsets
+				 */
+				delta = Math.min( Q.val1, R.val1 );
 
-			/**
-			 * Stitching the 2 presorted y-lists
-			 */
-			List<EPoint> recombine = new ArrayList<>( Q.val2.size() + R.val2.size() );
-			recombine.addAll( Q.val2 );
-			recombine.addAll( R.val2 );
-			recombine.sort( (o1, o2) -> Double.compare( o1.y, o2.y ) );
-
-			final double medianX = input.get( medianIndex ).x;
-			final int lowerXBound = (int) (medianX - delta);
-			final int upperXBound = (int) (medianX + delta);
-			
-			for (int i = 0; i < recombine.size(); i++)
-			{
-				EPoint pt = recombine.get( i );
-				if (pt.x <= lowerXBound || pt.x >= upperXBound){
-					continue;
-				}
-				Double dist;
-				for (int j = i + 1; j < (i + 1) + 15; j++)
+				/**
+				 * Stitching the 2 presorted y-lists
+				 */
+				List<EPoint> recombine = new ArrayList<EPoint>( Q.val2.size() + R.val2.size() );
+				ListIterator<EPoint> Qp = Q.val2.listIterator(), Rp = R.val2.listIterator();
+				EPoint qp, rp;
+				int qi = 0, ri = 0;
+				for (int i = 0; i < input.size(); i++)
 				{
-					if (j == recombine.size()) break;
-					dist = pt.distance( recombine.get( j ) );
-					if (dist == 0.0)
+					if (qi < Q.val2.size())
+					{
+						qp = Q.val2.get( qi );
+						if (ri < R.val2.size())
+						{
+							rp = R.val2.get( ri );
+							if (qp.y < rp.y)
+							{
+								recombine.add( qp );
+								qi++;
+							} else
+							{
+								recombine.add( rp );
+								ri++;
+							}
+						} else
+						{
+							recombine.add( qp );
+							qi++;
+						}
+					} else
+					{
+						if (ri < R.val2.size())
+						{
+							rp = R.val2.get( ri );
+							recombine.add( rp );
+							ri++;
+						}
+					}
+				}
+
+				final double medianX = input.get( medianIndex ).x;
+				final int lowerXBound = (int) (medianX - delta);
+				final int upperXBound = (int) (medianX + delta);
+
+				for (int i = 0; i < recombine.size(); i++)
+				{
+					EPoint pt = recombine.get( i );
+					if (pt.x <= lowerXBound || pt.x >= upperXBound)
 					{
 						continue;
 					}
-					delta = dist < delta ? dist : delta;
+					Double dist;
+					for (int j = i + 1; j < (i + 1) + 15; j++)
+					{
+						if (j == recombine.size()) { break; }
+						dist = pt.distance( recombine.get( j ) );
+						if (dist == 0.0)
+						{
+							continue;
+						}
+						delta = dist < delta ? dist : delta;
+					}
 				}
-			}
 
-			return new Tuple<>( delta, recombine );
+				return new Tuple<>( delta, recombine );
 		}
 	}
 
